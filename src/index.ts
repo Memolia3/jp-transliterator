a3cff6845b6f1eb1a3e4e7774e1db5852e12aa0f
Memolia <ultimum.memolia@gmail.com>
2025-02-01 19:12:44 +0900

    ディレクトリ名変更、型定義エクスポート

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as Romanizer } from "./features/romanizer";
 export { default as Japanizer } from "./features/japanizer"
+export type Combination = [string[], string[]];
+export type Combinations = Array<Combination>;
