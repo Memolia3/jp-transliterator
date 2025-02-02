2db1d2f96d06184533c5a7ec824524a3d3790a38
Memolia <ultimum.memolia@gmail.com>
2025-02-02 11:40:00 +0900

    Updates

--- data/pattern.d.ts
+++
@@ -1,4 +0,0 @@
-import type { TransliterationTable } from "../types/transliterate.types";
-export declare const kanaToRomajiMap: TransliterationTable;
-export declare const romajiToKanaMap: TransliterationTable;
-//# sourceMappingURL=pattern.d.ts.map
